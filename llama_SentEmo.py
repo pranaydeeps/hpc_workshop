0603a30cc0a4a8b156a98844c5af354752db5cf4
c1pher <pranaydeeps@gmail.com>
2026-04-15 12:03:00 +0200

    added a comment

--- llama_SentEmo.py
+++ llama_SentEmo.py
@@ -1,3 +1,5 @@
+
+#For HPC Workshop
 from transformers import AutoTokenizer, AutoModel
 from transformers.models.llama.modeling_llama import LlamaForCausalLM
 #from transformers import LlamaTokenizer
